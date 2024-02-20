e3d1850a74f5a937b8837a97ca72ffa18fafaef0
Rafi <rafirafi9550@gmail.com>
2024-02-19 19:04:43 -0500

    security impl.
    Added jwt security.

---
+++ src/main/java/com/cogent/ecommerce/security/JWTSecurityConfig.java
@@ -0,0 +1,148 @@
+package com.cogent.ecommerce.security;
+
+import ch.qos.logback.classic.encoder.JsonEncoder;
+import com.cogent.ecommerce.User.UserRepository;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
+@Configuration
+public class JWTSecurityConfig {
+
+
+
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+
+        //httpSecurity.authorizeHttpRequests((requests)->requests.anyRequest().authenticated());
+        //httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.sessionManagement(session->session.disable());
+        httpSecurity.csrf().disable();
+        httpSecurity.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        httpSecurity.authorizeRequests().requestMatchers("/signUp").permitAll()
+                .anyRequest().authenticated();
+        httpSecurity.authenticationProvider(authenticationProvider());
+
+        return httpSecurity.build();
+    }
+
+
+    @Autowired
+    private UserRepository userRepository;
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+//    @Bean
+//    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource){
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource){
+//
+////        var user2 = User.withUsername("default").password("{noop}random").roles("USER").build();
+////        var user = User.withUsername("rafi").password("{noop}admin").roles("ADMIN").build();
+//        var jdbcUserManager = new JdbcUserDetailsManager(dataSource);
+////        jdbcUserManager.createUser(user);
+////        jdbcUserManager.createUser(user2);
+//        return jdbcUserManager;
+//    }
+
+
+    //jwtDecoder
+    @Bean
+    public JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException {
+        return NimbusJwtDecoder.withPublicKey(rsaKey.toRSAPublicKey()).build();
+    }
+
+    //keyPairGenerator
+    @Bean
+    public KeyPair keyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    @Bean
+    public RSAKey rsaKey(KeyPair keyPair) throws NoSuchAlgorithmException {
+        return new RSAKey.Builder((RSAPublicKey) keyPair.getPublic()).privateKey(keyPair.getPrivate()).keyID(UUID.randomUUID().toString()).build();
+    }
+
+    @Bean
+    public RSAPublicKey rsaPublicKey(KeyPair keyPair){
+        return (RSAPublicKey) keyPair.getPublic();
+    }
+
+    @Bean
+    public RSAPrivateKey rsaPrivateKey(KeyPair keyPair){
+        return (RSAPrivateKey) keyPair.getPrivate();
+    }
+
+    //jwksource to generate json web tokens
+    @Bean
+    public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey){
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return ((jwkSelector, securityContext) -> jwkSelector.select(jwkSet));
+    }
+
+    //jwtencoder to create and encode the tokens
+    @Bean
+    public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwkSource){
+        return new NimbusJwtEncoder(jwkSource);
+    }
+
+
+}
