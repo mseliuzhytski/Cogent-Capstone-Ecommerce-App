c277c171e3953ec93c5f97c6665005da6589c7a1
Christopher Gioia <christophergioia64@gmail.com>
2024-03-04 11:00:34 -0800

    now a eureka client

--- src/main/java/com/cogent/ecommerce/EcommerceApplication.java
+++ src/main/java/com/cogent/ecommerce/EcommerceApplication.java
@@ -6,35 +6,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EcommerceApplication
 {
-//	@Value( "${spring.jpa.hibernate.ddl-auto}" )
-//	private String ddl_auto;
-//
-//	@Value("${seedData}")
-//	private String seedData;
-//	public static final String TRUE = "true";
-//	public static final String FALSE = "false";
-//
-//	public static final String CREATE_DROP = "create-drop";
-//	public static final String CREATE = "create";
-//	public static final String UPDATE = "update";
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
-
-//	@Autowired
-//	private DatabaseLoader loader;
-
-//	@Override
-//	public void run(String... args) throws Exception {
-//		if (seedData.equals(TRUE)) {
-//			loader.loadSeededData();
-//		}
-//	}
 
 }
