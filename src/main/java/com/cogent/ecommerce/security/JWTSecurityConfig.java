a42c9b1b68ec31673ced5ed758119a738964f6d3
Christopher Gioia <christophergioia64@gmail.com>
2024-03-06 16:43:10 -0800

    fix typo in displaying images

--- src/main/java/com/cogent/ecommerce/security/JWTSecurityConfig.java
+++ src/main/java/com/cogent/ecommerce/security/JWTSecurityConfig.java
@@ -55,7 +55,7 @@ public class JWTSecurityConfig {
         httpSecurity.csrf().disable();
         httpSecurity.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         httpSecurity.authorizeRequests().requestMatchers("/signUp","/products/getAllProducts","/products/getProduct/**",
-                        "/categories/getCategory","/resetPass/**","/product/list","/databaseloader", "/content**","/contact/**").permitAll()
+                        "/categories/getCategory","/resetPass/**","/product/list","/databaseloader", "/content/**","/contact/**").permitAll()
                 .anyRequest().authenticated();
         httpSecurity.authenticationProvider(authenticationProvider());
 
